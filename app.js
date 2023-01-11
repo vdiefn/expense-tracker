1f9f698e8892ceb276198de4116a554679ab4cce
vdiefn <ddhkxxhxx@hotmail.com>
2023-01-11 10:33:15 +0800

    feat: add delete and edit function

--- app.js
+++ app.js
@@ -51,6 +51,38 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+//修改
+app.get('/expenses/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Expense.findById(id)
+    .lean()
+    .then((expense) => res.render('edit', {expense}))
+    .catch(error => console.log(error))
+})
+app.post('/expenses/:id/edit', (req, res) => {
+  const id = req.params.id
+  const { name, date, category, amount} = req.body
+  return Expense.findById(id)
+    .then( expense => {
+      expense.name = name
+      expense.date = date
+      expense.category = category
+      expense.amount = amount
+      return expense.save()
+    })
+    .then(() => res.redirect('/'))
+    .catch( error => console.log(error)) 
+})
+
+//刪除
+app.post('/expenses/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Expense.findById(id)
+    .then(expense => expense.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
 })
